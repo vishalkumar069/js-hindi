561d6651e9dffa35de34b52139ad4e5772ee6810
Vishal Kumar <128383349+vishalkumar069@users.noreply.github.com>
2024-04-09 01:24:43 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using Nodejs, not browser so that this alert is not working here
+
+console.log(3 
+    +
+     3); //code readability that should be high
+
+console.log("Vishal");
+
+let name = "Vishal";
+let age = 24;
+let isLoghedIN = false;
+let state = "" //This is Undefined
+let state1 = null // Here thier is no value
+
+console.log(typeof state) // stae is undefined but it is string --> bcz of using "" it is string.
+console.log(typeof state1) // it is Object 
+
+/* Premitive Data Type */
+
+// number => 2 to the power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value --> means that variable is created and their is no value.
+// undefined => variable is created but value is not defined
+// Symbol => unique --> it is use to define uniqueness for make component in React
+
+
+/* Non-Premitive Data Type/Oject */
+
