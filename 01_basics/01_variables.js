16949f56772deface34bfe49fafb82b748fb25a1
Vishal Kumar <128383349+vishalkumar069@users.noreply.github.com>
2024-04-09 01:28:05 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "vishal.ptru.587@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ranchi"
+let acccountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "v.ptru.587@gmail.com"
+accountPassword = ""
