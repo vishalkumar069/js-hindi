b6f241306c692fd8a863f402321bd97bcea80677
Vishal Kumar <128383349+vishalkumar069@users.noreply.github.com>
2024-04-23 14:31:38 +0000

    dataTypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,26 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symboll, BigInt
+
+// Reference (Non-Primitive)
+
+//Array, Obejct, Functions
+
+// Array---
+const heros = ["Shaktiman", "Spiderman", "Superman"]
+
+//object---
+let myObj = {
+    name: "Vishal",
+    age: 24,
+}
+
+//Function---
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+//TO know the Datatypes---
+console.log(typeof heros)
+console.log(typeof myObj)
+console.log(typeof myFunction)
